108abe9be75aa924b77101f180910d00caab7423
miqbalnugraha <iqbalturmudzi@gmail.com>
2022-02-28 23:51:06 +0700

    static property and method

---
+++ 10static_method_property.ts
@@ -0,0 +1,32 @@
+class Ayam {
+    static kaki: number = 2;
+    static jalan() {
+        console.log(`ayam berjalan dengan ${this.kaki} kaki`);
+    }
+
+    getKaki() {
+        //console.log(this.kaki);
+        console.log(Ayam.kaki);
+    }
+}
+
+const ayam1 = new Ayam();
+const ayam2 = new Ayam();
+const ayam3 = new Ayam();
+const ayam4 = new Ayam();
+const ayam5 = new Ayam();
+ayam1.getKaki();
+ayam2.getKaki();
+ayam3.getKaki();
+ayam4.getKaki();
+ayam5.getKaki();
+Ayam.jalan();
+
+Ayam.kaki = 4; // mengubah static properti 
+
+ayam1.getKaki();
+ayam2.getKaki();
+ayam3.getKaki();
+ayam4.getKaki();
+ayam5.getKaki();
+Ayam.jalan();
