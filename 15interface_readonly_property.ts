4bc65a38f7457f2aa0d1309c408afd9a4c7ed0de
miqbalnugraha <iqbalturmudzi@gmail.com>
2022-03-02 15:47:03 +0700

    interface readonly property

---
+++ 15interface_readonly_property.ts
@@ -0,0 +1,7 @@
+interface Student {
+    readonly name: string;
+    readonly age: number;
+}
+
+let student: Student = {name: "Nugraha", age: 25};
+// ketika properti sudah di-assign value nya, value tersebut tidak bisa diubah lagi.
