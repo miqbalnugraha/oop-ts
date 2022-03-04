5f188fe18b8e6860e8f5598ae7db5a64c35c80b5
miqbalnugraha <iqbalturmudzi@gmail.com>
2022-03-04 19:09:36 +0700

    generics constraints

--- 24generic_constraints.ts
+++ 24generic_constraints.ts
@@ -15,4 +15,9 @@ function Generics24<T extends Length>(arg: T): T {
 const generics24 = Generics24("Nugraha");
 console.log(generics24);
 
+// jika ingin masukin number
+
+// error
+// const generics24a = Generics24(12312);
+
 const generics24a = Generics24({length: 20, value: 10});
